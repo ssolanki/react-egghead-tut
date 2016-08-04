68d168007f85c6ea7da627a1b1ba0fb41e791887
sahil <sahilsolanki07@gmail.com>
2016-08-04 17:59:38 +0530

    mounting component basic added

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import React from "react";
+import ReactDOM from "react-dom";
 class App extends React.Component {
-	render() {
-		return <Button> <Heart/> React <Heart/> </Button>;
+	constructor() {
+		super();
+		this.state = {value: 0};
+		this.update = this.update.bind(this);
+	}
+	update(){
+		this.setState({value: this.state.value +1});
+	}
+	// called once before mount
+	componentWillMount(){
+		console.log("mounting!");
+	}
+	render(){
+		console.log("rendering!");
+		return <button onClick={this.update}> {this.state.value} </button>;
+	}
+	// called once after mount
+	componentDidMount(){
+		console.log("component mounted!");
+	}
+	// when component is removed
+	componentWillUnmount(){
+		console.log("component removed!");
 	}
 }
-class Button extends React.Component {
-	render() {
-		return <button> {this.props.children} </button>;
+class Wrapper extends React.Component{
+	constructor(){
+		super();
+	}
+	mount(){
+		ReactDOM.render(<App />, document.getElementById("btn"));
+	}
+	unmount(){
+		ReactDOM.unmountComponentAtNode(document.getElementById("btn"));
+	}
+	render(){
+		return (
+			<div>
+				<button onClick={this.mount.bind(this)}> Mount </button>
+				<button onClick={this.unmount.bind(this)}> UnMount </button>
+				<div id = "btn"> </div>
+			</div>
+		);
 	}
 }
-//stateless Component
-const Heart = () => <span className = "glyphicon glyphicon-heart"> **</span>;
-
-export default App;
+export default Wrapper;
