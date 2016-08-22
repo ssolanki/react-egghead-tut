bbb1dfa5e0d526fa3160b3452e620d453094f028
sahil <sahilsolanki07@gmail.com>
2016-08-22 17:24:03 +0530

    dynamic component added

--- App.js
+++ App.js
@@ -1,77 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 class App extends React.Component{
 	constructor(){
 		super();
-		this.state = {red:0};
-		this.update = this.update.bind(this);
+		this.state = {
+			data: [
+				{id: 1, name: "sahil solanki"},
+				{id: 2, name: "rahul maurya"},
+				{id: 3, name: "nikhil"},
+				{id: 4, name: "ashish"}
+			]
+		};
 	}
-	update(){
-		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
+	render(){
+		let rows = this.state.data.map(person => {
+			return <PersonRow data = {person} />;
 		});
-	}
-	render(){
 		return (
-			<div>
-				<NumInput
-					ref="red"
-					min={0}
-					max={255}
-					step={1}
-					val={+this.state.red}
-					label="Red"
-					update={this.update}
-				/>
-				<NumInput
-					ref="red"
-					min={0}
-					max={255}
-					step={1}
-					val={+this.state.red}
-					label="Red"
-					type="number"
-					update={this.update}
-				/>
-			</div>
+			<table>
+				<tbody> {rows} </tbody>
+			</table>
 		);
 	}
 }
-class NumInput extends React.Component{
-	render(){
-		console.log(this.props.val)
-		let label = this.props.label !== ''?
-			<label>	{this.props.label} - {this.props.val}  </label>: '';
-		return (
-			<div>
-				<input ref='inp'
-					type = {this.props.type}
-					min = {this.props.min}
-					max = {this.props.max}
-					defaultValue = {this.props.val}
-					step = {this.props.step}
-					onChange = {this.props.update}
-				/>
-				{label}
-			</div>
-		);
-	}
-}
-NumInput.propTypes = {
-	min: React.PropTypes.number,
-	max: React.PropTypes.number,
-	step: React.PropTypes.number,
-	val: React.PropTypes.number,
-	label: React.PropTypes.string,
-	type: React.PropTypes.oneOf(['number','range']),
-	update: React.PropTypes.func.isRequired,
+const PersonRow = (props) => {
+	return(
+		<tr>
+			<td> {props.data.id} </td>
+			<td> {props.data.name} </td>
+		</tr>
+	);
 };
-NumInput.defaultProps = {
-	min: 0,
-	max: 25,
-	step: 1,
-	val: 0,
-	label: '',
-	type: 'range'
-};
-export default App;
+export default App ;
